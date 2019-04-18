dbbca8fb986cdc03f472ce904dfe052cc5a51f28
praveenkumar-s <praveenkumar.s@realimage.com>
2019-04-18 19:33:44 +0530

    Update IRController.ino
    Changed Char Buffer size to 250 to accomodate AC RAW CODES

--- src/IRController.ino
+++ src/IRController.ino
@@ -26,7 +26,7 @@ const int timeZone = -5;                                      // Timezone (-5 is
 
 const bool enableMDNSServices = true;                         // Use mDNS services, must be enabled for ArduinoOTA
 
-const unsigned int captureBufSize = 150;                      // Size of the IR capture buffer.
+const unsigned int captureBufSize = 250;                      // Size of the IR capture buffer.
 
 // WEMOS users may need to adjust pins for compatability
 const int pinr1 = 14;                                         // Receiving pin
